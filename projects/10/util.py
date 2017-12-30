2c47d64da9bee9d4c0ea570fba17a6b5e700df62
Karen Ang <thekarenang@gmail.com>
2017-12-30 20:46:51 +0800

    Week 10: ArrayTest passed

--- projects/10/util.py
+++ projects/10/util.py
@@ -11,3 +11,11 @@ class TokenType(Enum):
     IDENTIFIER = '[A-Za-z_][\w\d_]*'
     INT_CONST = '\d+'
     STR_CONST = '"[^\\n]*"'
+
+    def __str__(self):
+        if self == TokenType.INT_CONST:
+            return 'integerConstant'
+        elif self == TokenType.STR_CONST:
+            return 'stringConstant'
+        else:
+            return self.name.lower()
