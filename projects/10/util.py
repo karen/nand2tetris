216e5115a943cb5b221fa3529190cb7d0126b02c
Karen Ang <thekarenang@gmail.com>
2017-12-30 20:54:46 +0800

    Week 10: SquareGame Passed

--- projects/10/util.py
+++ projects/10/util.py
@@ -7,7 +7,7 @@ class TokenType(Enum):
     KEYWORD = ('class|constructor|function|method|field|static|var|int'
                '|char|boolean|void|true|false|null|this|let|do|if|else'
                '|while|return')
-    SYMBOL = '[' + re.escape('{}()[].,;+-*/&|<>=-') + ']'
+    SYMBOL = '[' + re.escape('{}()[].,;+-*/&|<>=-~') + ']'
     IDENTIFIER = '[A-Za-z_][\w\d_]*'
     INT_CONST = '\d+'
     STR_CONST = '"[^\\n]*"'
